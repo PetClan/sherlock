81193a7a08788e4e4ffe69fa7337d30412564807
Angela <angela@petclan.ai>
2025-12-07 22:42:41 +0000

    Add monitoring router to API init

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -8,6 +8,7 @@ from app.api.routers.auth import router as auth_router
 from app.api.routers.reddit import router as reddit_router
 from app.api.routers.reports import router as reports_router
 from app.api.routers.google_router import router as google_router
+from app.api.routers.monitoring import router as monitoring_router
 
 
 # Main API router
@@ -18,3 +19,4 @@ api_router.include_router(auth_router)
 api_router.include_router(reddit_router)
 api_router.include_router(reports_router)
 api_router.include_router(google_router)
+api_router.include_router(monitoring_router)
